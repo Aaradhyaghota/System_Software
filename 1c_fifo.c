df098b2c9b7a6a02bb27cd47ba7e40c4765fdc3e
Aaradhyaghota <aaradhyaghota@gmail.com>
2024-08-14 22:11:44 +0530

    some changes made to 1c ,3, 4 and deleted some file

--- 1c_fifo.c
+++ 1c_fifo.c
@@ -1,10 +1,16 @@
 #include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-	int flag = mknod("1c_fifo",744,0);
-	if(flag == 0)printf("Succesful\n");
-	else printf("Unsuccesful\n");
+int main(int argc, char ** argv){
+	if(argc < 2){
+		printf("Error- Input Valid input");
+		exit(0);
+	}
+	int flag = mknod(argv[1], S_IFIFO | 0666, 0);
+	if(flag == 0)printf("------------- Succesfully Created fifo named '%s' ------------\n",argv[1]);
+	else printf("Fifo creation Unsuccesful\n");
+	
 
 return 0;
 }
--- 3_crtfile.c
+++ 3_crtfile.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-int main(){
+int main(int argc, char** argv){
 	//      int creat(const char *pathname, mode_t mode);
 	//
 	//
-	int fd  = creat("newfile", 0774);
+	int fd  = creat(argv[1], 0774);
 	if(fd>0)printf("Succesful\n");
 	else printf("Unsuccesful\n");
-	printf("%d \n", fd);
+	printf("File discripter value is %d \n", fd);
 
 
 return 0;

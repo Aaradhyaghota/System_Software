4d7a952a4dcf10ab02c3f0dfcbc93c0f728cbb3a
Aaradhyaghota <aaradhyaghota@gmail.com>
2024-10-15 01:13:14 +0530

    changed data tyoe of amount

--- MINI_Project/record structures/loan.h
+++ MINI_Project/record structures/loan.h
@@ -3,10 +3,10 @@
 
 struct Loan {
     int id;  // 1, 2, 3 , 4
-    long int amount;
-    int customer_account_number;
+    int amount;
+    int customerID;
     int status;  // 0-> pending (manager),1->employee , 2-> loan approved(customer), 3->loan rejected(customer)
-    //bool assigned;
+    // bool assigned;
 };
 
 #endif
